8cd1b73e68a0de48f5a2abaff3fe1bece17ba0b2
Giles Goddard <giles@vitei.com>
2014-01-23 18:53:13 +0900

    fixed passthrough bug

--- lib/git-media/filter-smudge.rb
+++ lib/git-media/filter-smudge.rb
@@ -7,8 +7,9 @@ module GitMedia
       #can_download = false # TODO: read this from config and implement
       
       # read checksum size
-      sha = STDIN.readpartial(64).lines.first.chomp # read no more than 64 bytes
-      if STDIN.eof? && sha.length == 40 && sha.match(/^[0-9a-fA-F]+$/) != nil
+      sha = STDIN.readpartial(40)
+  
+      if sha.length == 40 && sha.match(/^[0-9a-fA-F]+$/) != nil
         # this is a media file
         media_file = File.join(media_buffer, sha.chomp)
         if File.exists?(media_file)
@@ -35,11 +36,13 @@ module GitMedia
         end
       else
         # if it is not a 40 character long hash, just output
-        STDERR.puts('Unknown git-media file format')
+        #STDERR.puts('Unknown git-media file format')
         print sha
         while data = STDIN.read(4096)
           print data
         end
+        #STDERR.puts('Expected a stub file : '+sha)
+        #exit(1)
       end
     end
 
